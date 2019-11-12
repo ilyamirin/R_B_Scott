26bb8593ceadc073a6d495a3f914a9ee459d1b7e
Alexey Mogilyovkin <zeroicq@gmail.com>
2019-11-12 13:15:32 +1000

    Improve wav decode.

--- gena/main.py
+++ gena/main.py
@@ -4,7 +4,7 @@ import logging
 import sys
 import librosa
 
-DATA_DIR = './data_dir/'
+DATA_DIR = './data_dir_wav/'
 
 logger = logging.getLogger("gena")
 
@@ -21,23 +21,19 @@ def configure_logger():
     logger.addHandler(handler)
 
 
-def path_to_file(path):
-    return tf.py_function(lambda x: librosa.load(str(x.numpy(), 'utf-8')), [path], [tf.float32])
-    # return path
-    # return librosa.load(str(path))
+def decode_wav(filename):
+    file = tf.io.read_file(filename)
+    return tf.audio.decode_wav(file)
 
 
 def main():
     args = parse_arguments()
     configure_logger()
 
-    logger.debug("Start preparing dataset")
-    dataset = tf.data.Dataset.list_files(args.data_dir + '*mp3')
-    logger.debug("Map")
-    dataset = dataset.map(path_to_file)
-    logger.debug("Finished")
-    for d in dataset.take(2):
-        print(d[0].numpy().max())
+    dataset = tf.data.Dataset.list_files(args.data_dir + '*.wav')
+    dataset = dataset.map(decode_wav)
+    for d in dataset.take(5):
+        print(d[0])
         print('\n')
 
 
