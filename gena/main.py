ca9d56e4be8bd68811b7ab413aea7c256df2c166
Alexey Mogilyovkin <zeroicq@gmail.com>
2019-11-12 22:47:03 +1000

    Add log.

--- gena/main.py
+++ gena/main.py
@@ -12,7 +12,7 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 DATA_DIR = './data_dir_wav/'
 # tracks to process simultaneously
-BATCH_SIZE = 3
+BATCH_SIZE = 10
 # size of input
 SAMPLE_SIZE = 44200//100
 
@@ -21,16 +21,15 @@ logger = logging.getLogger("gena")
 
 class NBatchLogger(tf.keras.callbacks.Callback):
     def __init__(self, display=100):
-        '''
-        display: Number of batches to wait before outputting loss
-        '''
         self.seen = 0
         self.display = display
 
     def on_train_batch_end(self, batch, logs={}):
         self.seen += logs.get('size', 0)
+
         if self.seen % self.display == 0:
-            print('\n{0} - Batch Loss: {1}'.format(self.seen, self.params['metrics']['loss']))
+            print('\n{0} - Batch Loss: {1}'.format(self.seen, logs['loss']))
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Description")
@@ -49,19 +48,16 @@ def decode_wav(filename):
     return tf.audio.decode_wav(file, desired_channels=1)
 
 
-def print_dataset(dataset):
-    print("Printing " + str(dataset))
-    start = 0
-    end = 0
-    for d in dataset.take(100):
-        end = timer()
-        print(end - start)
-        print("hello")
-        start = timer()
+# def print_dataset(dataset):
+#     print("Printing " + str(dataset))
+#     start = 0
+#     end = 0
+#     for d in dataset.take(100):
+#         end = timer()
+#         print(end - start)
+#         print("hello")
+#         start = timer()
 
-
-    # print('------------------------------')
-    # print('\n\n')
 
 
 def main():
@@ -74,21 +70,19 @@ def main():
     # drop sample rate
     dataset = dataset.map(lambda t: t[0])
     dataset = dataset.unbatch()
-    dataset = dataset.batch(SAMPLE_SIZE, drop_remainder=True).batch(BATCH_SIZE).repeat(10).prefetch(tf.data.experimental.AUTOTUNE)
+    dataset = dataset.batch(SAMPLE_SIZE, drop_remainder=True).batch(BATCH_SIZE).prefetch(tf.data.experimental.AUTOTUNE)
     dataset = dataset.zip((dataset, dataset))
 
     model = keras.Sequential([
         keras.layers.LSTM(SAMPLE_SIZE, input_shape=(None, 1), return_sequences=True, dropout=.3),
-        keras.layers.LSTM(SAMPLE_SIZE // 2, return_sequences=True, dropout=.3),
         keras.layers.LSTM(SAMPLE_SIZE // 4, return_sequences=True, dropout=.3),
-        keras.layers.LSTM(SAMPLE_SIZE // 2, return_sequences=True, dropout=.3),
+        keras.layers.LSTM(SAMPLE_SIZE // 16, return_sequences=True, dropout=.3),
+        keras.layers.LSTM(SAMPLE_SIZE // 4, return_sequences=True, dropout=.3),
         keras.layers.Dense(SAMPLE_SIZE),
     ])
-
-    out_batch = NBatchLogger(display=1)
-
+    log = NBatchLogger()
     model.compile(optimizer='adam', loss='mean_absolute_error')
-    model.fit(dataset, epochs=10000, verbose=2, steps_per_epoch=1, callbacks=[])
+    model.fit(dataset, epochs=2, verbose=2, steps_per_epoch=None, callbacks=[log])
 
 
 if __name__ == '__main__':
