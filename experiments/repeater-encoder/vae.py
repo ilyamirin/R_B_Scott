c8288b09246c4326546fbdaf639f9d08219b0289
Boris Timofeenko <tmfn@feip.co>
2019-12-06 18:28:26 +1000

    Refactor

--- experiments/repeater-encoder/vae.py
+++ experiments/repeater-encoder/vae.py
@@ -11,6 +11,9 @@ from keras import backend as K
 import numpy as np
 import matplotlib.pyplot as plt
 import pypianoroll_midi
+import os
+
+MODEL_DIR = "model"
 
 # reparameterization trick
 # instead of sampling from Q(z|X), sample epsilon = N(0,I)
@@ -32,85 +35,87 @@ def sampling(args):
     epsilon = K.random_normal(shape=(batch, dim))
     return z_mean + K.exp(0.5 * z_log_var) * epsilon
 
-dataset = pypianoroll_midi.load_dataset()
+def train():
+    dataset = pypianoroll_midi.load_dataset()
+    songs_number = dataset.shape[0]
+    song_length_in_bars = dataset.shape[1]
+    song_tracks = dataset.shape[2]
+    grid_size = dataset.shape[3]
+    midi_notes_number = dataset.shape[4]
+    dataset = dataset.reshape((songs_number * song_length_in_bars, song_tracks * grid_size * midi_notes_number))
+    x_train, x_test = np.array_split(dataset, 2)
+    original_dim = x_train.shape[1]
 
-#rewrite shapes
-shape1 = dataset.shape[2]
-shape2 = dataset.shape[3]
-shape3 = dataset.shape[4]
-dataset = dataset.reshape((dataset.shape[0] * dataset.shape[1], dataset.shape[2] * dataset.shape[3] * dataset.shape[4]))
+    #normalize
+    x_train = x_train.astype('float32') / midi_notes_number
+    x_test = x_test.astype('float32') / midi_notes_number
 
-# MNIST dataset
-x_train, x_test = np.array_split(dataset, 2)
+    input_shape = (original_dim,)
+    intermediate_dim = 512
+    batch_size = 128
+    latent_dim = 2
+    epochs = 1
 
-original_dim = x_train.shape[1]
-x_train = x_train.astype('float32') / 128
-x_test = x_test.astype('float32') / 128
+    # VAE model = encoder + decoder
+    # build encoder model
+    inputs = Input(shape=input_shape, name='encoder_input')
+    x = Dense(intermediate_dim, activation='relu')(inputs)
+    z_mean = Dense(latent_dim, name='z_mean')(x)
+    z_log_var = Dense(latent_dim, name='z_log_var')(x)
 
-# network parameters
-input_shape = (original_dim, )
-intermediate_dim = 512
-batch_size = 128
-latent_dim = 2
-epochs = 2000
+    # use reparameterization trick to push the sampling out as input
+    # note that "output_shape" isn't necessary with the TensorFlow backend
+    z = Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
 
-# VAE model = encoder + decoder
-# build encoder model
-inputs = Input(shape=input_shape, name='encoder_input')
-x = Dense(intermediate_dim, activation='relu')(inputs)
-z_mean = Dense(latent_dim, name='z_mean')(x)
-z_log_var = Dense(latent_dim, name='z_log_var')(x)
+    # instantiate encoder model
+    encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
+    encoder.summary()
+    plot_model(encoder, to_file='vae_mlp_encoder.png', show_shapes=True)
 
-# use reparameterization trick to push the sampling out as input
-# note that "output_shape" isn't necessary with the TensorFlow backend
-z = Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
+    # build decoder model
+    latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
+    x = Dense(intermediate_dim, activation='relu')(latent_inputs)
+    outputs = Dense(original_dim, activation='sigmoid')(x)
 
-# instantiate encoder model
-encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
-encoder.summary()
-plot_model(encoder, to_file='vae_mlp_encoder.png', show_shapes=True)
+    # instantiate decoder model
+    decoder = Model(latent_inputs, outputs, name='decoder')
+    decoder.summary()
+    plot_model(decoder, to_file='vae_mlp_decoder.png', show_shapes=True)
 
-# build decoder model
-latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
-x = Dense(intermediate_dim, activation='relu')(latent_inputs)
-outputs = Dense(original_dim, activation='sigmoid')(x)
+    # instantiate VAE model
+    outputs = decoder(encoder(inputs)[2])
+    vae = Model(inputs, outputs, name='vae_mlp')
 
-# instantiate decoder model
-decoder = Model(latent_inputs, outputs, name='decoder')
-decoder.summary()
-plot_model(decoder, to_file='vae_mlp_decoder.png', show_shapes=True)
+    models = (encoder, decoder)
 
-# instantiate VAE model
-outputs = decoder(encoder(inputs)[2])
-vae = Model(inputs, outputs, name='vae_mlp')
+    # VAE loss = mse_loss or xent_loss + kl_loss
+    reconstruction_loss = mse(inputs, outputs)
 
-models = (encoder, decoder)
-
-# VAE loss = mse_loss or xent_loss + kl_loss
-reconstruction_loss = mse(inputs, outputs)
-
-reconstruction_loss *= original_dim
-kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
-kl_loss = K.sum(kl_loss, axis=-1)
-kl_loss *= -0.5
-vae_loss = K.mean(reconstruction_loss + kl_loss)
-vae.add_loss(vae_loss)
-vae.compile(optimizer='adam')
-vae.summary()
-plot_model(vae,
-           to_file='vae_mlp.png',
-           show_shapes=True)
+    reconstruction_loss *= original_dim
+    kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
+    kl_loss = K.sum(kl_loss, axis=-1)
+    kl_loss *= -0.5
+    vae_loss = K.mean(reconstruction_loss + kl_loss)
+    vae.add_loss(vae_loss)
+    vae.compile(optimizer='adam')
+    vae.summary()
+    plot_model(vae,
+               to_file='vae_mlp.png',
+               show_shapes=True)
     # train the autoencoder
-vae.fit(x_train,
-        epochs=epochs,
-        batch_size=batch_size,
-        validation_data=(x_test, None))
-vae.save_weights('vae_mlp_mnist.h5')
+    vae.fit(x_train,
+            epochs=epochs,
+            batch_size=batch_size,
+            validation_data=(x_test, None))
+    if not os.path.exists(MODEL_DIR):
+        os.mkdir(MODEL_DIR)
+    vae.save_weights(os.path.join(MODEL_DIR, 'model.h5'))
+    z_sample = np.array([[0, 0]])
+    x_decoded = decoder.predict(z_sample)
+    x_decoded = (x_decoded * 128).astype('int')
+    x_decoded = x_decoded.reshape(song_tracks, grid_size, midi_notes_number)
+    x_decoded = np.expand_dims(x_decoded, axis=0)
+    pypianoroll_midi.write_song_to_midi(x_decoded, "output.midi")
+    print(x_decoded)
 
-z_sample = np.array([[0, 0]])
-x_decoded = decoder.predict(z_sample)
-x_decoded = (x_decoded * 128).astype('int')
-x_decoded = x_decoded.reshape(shape1, shape2, shape3)
-x_decoded = np.expand_dims(x_decoded, axis=0)
-pypianoroll_midi.write_song_to_midi(x_decoded, "output.midi")
-print(x_decoded)
+train()
