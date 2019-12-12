0a8e96aa7f9b8e178cb1a6f1da215046924ee2d3
Alexey Mogilyovkin <zeroicq@gmail.com>
2019-12-12 15:04:15 +1000

    Add formatter to logger.

--- gena/logger.py
+++ gena/logger.py
@@ -6,6 +6,9 @@ class Logger(logging.Logger):
     def __init__(self, name, level=logging.NOTSET):
         super().__init__(name, level)
         handler = logging.StreamHandler(sys.stdout)
+        # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        formatter = logging.Formatter('%(name)s:%(levelname)s: %(message)s')
+        handler.setFormatter(formatter)
         self.addHandler(handler)
 
     def line_separator(self,):
